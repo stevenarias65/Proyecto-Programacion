574ab1855f244fcb28aec1516a1b1fbb192fddaa
stevenarias65 <44099803+stevenarias65@users.noreply.github.com>
2021-07-07 17:43:12 -0600

    commit

--- Proyecto de programacion I/src/proyecto/de/programacion/i/NewJFrame1.java
+++ Proyecto de programacion I/src/proyecto/de/programacion/i/NewJFrame1.java
@@ -69,6 +69,7 @@ public class NewJFrame1 extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(NewJFrame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
