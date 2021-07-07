69068b7b2bf9412e1b665ee2b4b0c03eaddfba9f
stevenarias65 <stevenarias65@gmail.com>
2021-07-07 17:07:30 -0600

    Ronald Arias
    Creacion de Proyecto

---
+++ Proyecto de programacion I/src/proyecto/de/programacion/i/ProyectoDeProgramacionI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.de.programacion.i;
+
+/**
+ *
+ * @author steven
+ */
+public class ProyectoDeProgramacionI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
